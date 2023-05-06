e2d9a9cdf57eceb3fbdbb17675b2633db1c4dcc0
neipr3s <129725761+neipr3s@users.noreply.github.com>
2023-05-06 22:14:57 +0200

    Removed existing code and replaced with a new variable 'pokemonList' and added several objects to to a new array containing 3 different types of pokemon with 3 keys each (name, type & height)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-alert('Hello world');
-let favouriteFood = 'safawi dates';
-document.write(favouriteFood)
+let pokemonList = [
+    {
+    name: "Bulbasaur",
+    type: "Grass/Poison",
+    height: 15
+  },
+  {
+    name: "Charmander",
+    type: "Fire",
+    height: 12
+  },
+  {
+    name: "Squirtle",
+    type: "Water",
+    height: 10
+  }];
