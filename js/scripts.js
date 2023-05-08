19514b79ec17a3bfa8763402408e9ec3fc2a7878
neipr3s <129725761+neipr3s@users.noreply.github.com>
2023-05-08 19:17:57 +0200

    added code to create a loop to iterate over each item in the pokemonList array creating a list on webpage & display the type and height of the pokemon next to the name as well as a special message if the pokemon's height is over 1.5 using a condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [  
   {    
     name: "Bulbasaur",    
-    type: ["Grass", "Poison"],
+    type: ["Grass", " Poison" ],
     height: 1.8
   },
   {
@@ -12,6 +12,22 @@ let pokemonList = [
   {
     name: "Squirtle",
     type: ["Water"],
-    height: 1.0
+    height: 1
   }
 ];
+
+// Iterate over each item in the pokemonList array
+for (let i = 0; i<pokemonList.length; i++) 
+// Check if the Pokémon's height is greater than 1.5
+  if (pokemonList[i].height > 1.5){
+    // If so, display a special message
+    document.write(pokemonList[i].name + " [type: " + pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ") - Wow, That's Big! <br><br>")
+  } 
+  else {
+  // if not, just display the name and height without comment
+    document.write(pokemonList[i].name + " [type: "+ pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ")<br><br>")// [")<br><br>" closes bracket and creates space with 2 new lines];
+
+}
+
+
+  
