f1844beecbec21bb9b783506a95998e40eb07289
neipr3s <129725761+neipr3s@users.noreply.github.com>
2023-05-11 19:51:51 +0200

    swapped out for loop for forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,33 @@ let pokemonList = [
   }
 ];
 
+pokemonList.forEach(function(pokemon){
+  // Check if the Pokémon's height is greater than 1.5
+  if (pokemon.height > 1.5){
+    // If so, display a special message
+    document.write("<p>" + pokemon.name + " [type: " + pokemon.type + "]" + " (height: " + pokemon.height + ") - Wow, That's Big! <br><br></p>")
+  } 
+  else {
+  // if not, just display the name and height without comment
+    document.write("<p>" + pokemon.name + " [type: "+ pokemon.type + "]" + " (height: " + pokemon.height + ")<br><br></p>")// [")<br><br>" closes bracket and creates space with 2 new lines];
+
+}
+});
+
+/*
 // Iterate over each item in the pokemonList array
 for (let i = 0; i<pokemonList.length; i++) 
 // Check if the Pokémon's height is greater than 1.5
   if (pokemonList[i].height > 1.5){
     // If so, display a special message
-    document.write(pokemonList[i].name + " [type: " + pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ") - Wow, That's Big! <br><br>")
+    document.write("<p>" + pokemonList[i].name + " [type: " + pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ") - Wow, That's Big! <br><br></p>")
   } 
   else {
   // if not, just display the name and height without comment
-    document.write(pokemonList[i].name + " [type: "+ pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ")<br><br>")// [")<br><br>" closes bracket and creates space with 2 new lines];
+    document.write("<p>" + pokemonList[i].name + " [type: "+ pokemonList[i].type + "]" + " (height: " + pokemonList[i].height + ")<br><br></p>")// [")<br><br>" closes bracket and creates space with 2 new lines];
 
 }
+*/
 
 
   
