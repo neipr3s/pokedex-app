9db4c796c06354ef2ed6bbb0784eb22679226169
neipr3s <129725761+neipr3s@users.noreply.github.com>
2023-05-29 13:17:29 +0200

    see description for multiple changes
    - removed previous pokemon list with external api containing list of all pokemon (note: limit = 150)
    
    - added loadList function which fetches complete list of pokemon from external API
    
    -  updated 'add' function using name and detailsUrl keys to retrieve pokemon from results and add to list
    
    - added loadDetails function  to display imageUrl, height and types in console log.
    
    - Edited showDetails() function to load the Pokémon details from the API instead of loading static data
    
    - changed external call functions to make sure the Pokémon list is only rendered after  all information is retrieved from the server
    
    - finally, added functions to display loading message while data is being retrieved from server.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,24 @@
-
 let pokemonRepository = (function() {
-  let pokemonList = [  
-  {    
-    name: "Bulbasaur",    
-    type: ["Grass", " Poison" ],
-    height: 1.8
-  },
-  {
-    name: "Charmander",
-    type: ["Fire"],
-    height: 1.2
-  },
-  {
-    name: "Squirtle",
-    type: ["Water"],
-    height: 1
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=150';
+
+ function add(pokemon) { 
+  if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "detailsUrl" in pokemon
+  ) {
+    pokemonList.push(pokemon);
+  } else {
+    console.log("pokemon is not correct")
   }
- ];
+ }
  // allows external code to access the pokemonList //
  function getAll() {
   return pokemonList;
  }
-
- // this function is used to add new pokemon to list //
- // it takes a pokemon parameter which should be an object with properties 'name' 'type' 'height' //
- // it first checks if pokemon parameter has all correct keys & if all conditions are met it will push the new pokemon to the pokemonList array //
- function add(pokemon) { 
-  const expectedKeys = ['name', 'type', 'height']; // keys //
-  const actualKeys = Object.keys(pokemon);
-
-  }
- // actual keys are compared with the expectedKeys array to check if pokemon object has all expected keys //
- // typeof pokemon === 'object' checks if pokemon variable is an object ensuring only objects can be added to list // 
- // expectedKeys.every(key => actualKeys.includes(key)) uses the every message on 'expected keys' to check if expectedKeys array is present in actualKeys array //
-
-  if(typeof pokemon === 'object' && expectedKeys.every(key => actualKeys.includes(key))){
-  pokemonList.push(pokemon);
-  } else { // if conditions arent met display following message //
-    console.error('You can only add an object to the pokemon list')
-  }
-
   function findPokemonByName(name)//parameter//
-   { // returns an array of pokemon objects that match with the 'name' parameter which represents the name of the pokemon you want to find//
+{ // returns an array of pokemon objects that match with the 'name' parameter which represents the name of the pokemon you want to find//
      // pokemonList.filter iterates over each pokemon object //
      // uses callback function (function(pokemon) {return pokemon.name() === name(); which is envoked for each object in the pokemonList array.//
      // it takes a pokemon parameter representing each pokemon in the array. 'pokemon.name() === name()' is used to check if the 'name' property of the current pokemon is equal to the given name parameter//
@@ -53,28 +30,70 @@ let pokemonRepository = (function() {
     return filteredPokemon;
   }
 
- function showDetails(pokemon){
- console.log(pokemon)
- }
-
  function addListItem(pokemon){
   let element = document.querySelector('.pokemon-List');
   let listItem = document.createElement('li');
   let button = document.createElement('button');
-
   button.innerText = pokemon.name; 
   button.classList.add('button-style');
-  
   listItem.appendChild(button); // appends the button as a child to the list item //
   element.appendChild(listItem); // appends the list item as a child to the container element// 
-  
   addButtonEventListener(button, pokemon);
+ }
 
-}
+ function showLoadingMessage() { // Create and append a loading message
+  let loadingMessage = document.createElement('p');
+  loadingMessage.textContent = 'Loading...'
+  document.body.appendChild(loadingMessage);
+ }
 
-  
-  
-function addButtonEventListener(button, pokemon){
+ function hideLoadingMessage(){
+  let loadingMessage = document.querySelector('p');
+  if (loadingMessage){
+    loadingMessage.remove();
+  }
+ }
+ 
+ function loadList() {
+  showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    })
+  }
+
+  function loadDetails(item) {
+    showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+ }
+    
+   function showDetails(pokemon){
+    loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+    }); 
+  }
+ 
+ function addButtonEventListener(button, pokemon){
   // Event listener for button click  
   button.addEventListener('click', function() {
     // Call showDetails function with the pokemon object as the parameter
@@ -82,20 +101,24 @@ function addButtonEventListener(button, pokemon){
   });
  }
 
- return { // the purpose of this, is to expose the given functions within pokemonRepository so that external code can access and use these functions while keeping pokemonList private //
+ return { 
   getAll: getAll,
   add: add,
   findPokemonByName: findPokemonByName,
   addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  showDetails: showDetails  
  };
-
- 
-
 })();
 
+
 // Iterate over the pokemonList and add each pokemon as a list item
-pokemonRepository.getAll().forEach(function(pokemon){
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+ pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon); 
+ });
+
 });
 
 
